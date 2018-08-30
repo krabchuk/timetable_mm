4bec7bb70f2fd577fa192594f0252312cbec70c4
Nikita Kravchuk <krabchuk@gmail.com>
2018-08-30 19:39:10 +0300

    NEW: added database for users

---
+++ database.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+table_sql = """CREATE TABLE IF NOT EXISTS users (id integer, group_num integer)"""
+
+insert_sql = """INSERT INTO users VALUES (?,?)"""
+
+get_data_by_id_sql = """SELECT * FROM users WHERE id=?"""
+
+db_file = "./databases/database_users"
+
+
+class DataStorage:
+    def __init__(self, db_file, table_sql):
+        self.db = sqlite3.connect(db_file, isolation_level=None)
+        self.db.cursor().execute(table_sql)
+
+    def __del__(self):
+        self.db.close()
+
+    def add_user(self, user_id, user_group):
+        c = self.db.cursor()
+        c.execute(insert_sql, (user_id, user_group))
+
+    def user_exist(self, user_id):
+        c = self.db.cursor()
+        c.execute(get_data_by_id_sql, (user_id,))
+        res = c.fetchall()
+        if len(res) > 0:
+            return True
+        else:
+            return False
+
+    def get_users_group(self, user_id):
+        c = self.db.cursor()
+        c.execute(get_data_by_id_sql, (user_id,))
+        if c.rowcount == 0:
+            return 0
+        else:
+            data = c.fetchall()
+            return data[0].group_num
+
+
+class SmallStorage:
+    def __init__(self):
+        self.data = set()
+
+    def add_id(self, user_id):
+        self.data.add(user_id)
+
+    def rm_id(self, user_id):
+        self.data.remove(user_id)
+
+    def exist(self, user_id):
+        if user_id in self.data:
+            return True
+        else:
+            return False
+
+
+db = DataStorage(db_file, table_sql)
+small_db = SmallStorage()
--- main.py
+++ main.py
@@ -1,20 +1,60 @@
 import telebot
 from telebot import apihelper
+from telebot import types
 import tokens
+import database
+import time
 
 
 bot = telebot.TeleBot(tokens.token, threaded=False)
 apihelper.proxy = {'https': 'socks5://' + str(tokens.proxy)}
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_message(chat_id=message.chat.id, text='Test message', reply_markup=types.ReplyKeyboardRemove())
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_message(message):
-    bot.send_message(message.chat.id, message.text)
 
+@bot.message_handler(commands=['chose_course'])
+def chose_course(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('1 курс')
+    btn2 = types.KeyboardButton('2 курс')
+    btn3 = types.KeyboardButton('3 курс')
+    btn4 = types.KeyboardButton('4 курс')
+    btn5 = types.KeyboardButton('5 курс')
+    btn6 = types.KeyboardButton('6 курс')
+    markup.add(btn1, btn2, btn3,
+               btn4, btn5, btn6)
+    bot.send_message(chat_id=message.chat.id, text='Выбери свой курс:', reply_markup=markup)
+
+
+@bot.message_handler(commands=['start'])
+def add_group_begin(message):
+    database.small_db.add_id(message.chat.id)
+    bot.send_message(chat_id=message.chat.id, text='Отправь номер группы:')
+
+
+@bot.message_handler(func=lambda message: database.small_db.exist(message.chat.id))
+def add_group_end(message):
+    database.small_db.rm_id(message.chat.id)
+    database.db.add_user(message.chat.id, int(message.text))
+    bot.send_message(chat_id=message.chat.id, text='Номер {} добавлен'.format(int(message.text)))
+
+
+@bot.message_handler(commands=['check'])
+def check_user_id(message):
+    if database.db.user_exist(message.chat.id):
+        bot.send_message(chat_id=message.chat.id, text='Вы зарегистрированны')
+    else:
+        bot.send_message(chat_id=message.chat.id, text='Вы ещё не зарегистрировались. Отправьте команду /start')
+
+# while True:
+#     try:
+#         bot.polling(none_stop=True)
+#     except Exception:
+#         print('Connection error, restart in 1 sec')
+#         time.sleep(1)
 
 bot.polling(none_stop=True)
