a5a95f22004d0fd116c8f9d2d11993ca9a6567d0
krabchuk <krabchuk@gmail.com>
2018-09-04 01:30:56 +0300

    ENH: tnx for Yana

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ def send_today(message):
 @bot.message_handler(commands=['info'])
 def send_info(message):
     info_text = """Timetable MM bot: v1.0
-    Благодарность Кириллу Сапунову за помощь в парсинге расписания.
+    Благодарности:
+    Кириллу Сапунову за помощь в парсинге расписания,
+    Яне Нагорных за подготовку аватара, дизайн и за то, что она лучшая староста в мире:3
 
 Отзывы, пожелания по работе бота, баги и несоответствия в расписании присылайте на @krabchuk"""
     bot.send_message(chat_id=message.chat.id, text=info_text)
