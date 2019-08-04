edfa981b842d5e32c4e801c3d66e329bbe058b95
krabchuk <krabchuk@gmail.com>
2019-08-04 21:29:28 +0300

    NEW: Replase sqlite with mongodb

--- database.py
+++ database.py
@@ -1,46 +1,28 @@
-import sqlite3
-
-table_sql = 'CREATE TABLE IF NOT EXISTS users (id integer, group_num integer)'
-
-insert_sql = 'INSERT INTO users VALUES (?,?)'
-
-get_data_by_id_sql = 'SELECT * FROM users WHERE id=?'
-
-delete_by_id_sql = 'DELETE FROM users WHERE id=?'
-
-db_file = './database_users'
+from pymongo import MongoClient
 
 
 class DataStorage:
-    def __init__(self, db_file, table_sql):
-        self.db = sqlite3.connect(db_file, isolation_level=None)
-        self.db.cursor().execute(table_sql)
-
-    def __del__(self):
-        self.db.close()
+    def __init__(self):
+        client = MongoClient(port=27017)
+        self.db = client.databse
 
     def add_user(self, user_id, user_group):
-        c = self.db.cursor()
-        c.execute(delete_by_id_sql, (user_id,))
-        c.execute(insert_sql, (user_id, user_group))
+        self.db.users.delete_many({'id': user_id})
+        self.db.users.insert_one({'id': user_id, 'group': user_group})
 
     def user_exist(self, user_id):
-        c = self.db.cursor()
-        c.execute(get_data_by_id_sql, (user_id,))
-        res = c.fetchall()
-        if len(res) > 0:
+        user = self.db.users.find_one({'id': user_id})
+        if user:
             return True
         else:
             return False
 
     def get_users_group(self, user_id):
-        c = self.db.cursor()
-        c.execute(get_data_by_id_sql, (user_id,))
-        if c.rowcount == 0:
-            return 0
+        user = self.db.users.find_one({'id': user_id})
+        if user:
+            return user.get('group')
         else:
-            data = c.fetchall()
-            return data[0][1]
+            return 0
 
 
 class SmallStorage:
@@ -73,5 +55,5 @@ class TotalOwnTimetablesStorage:
 
 
 tt_storage = TotalOwnTimetablesStorage()
-db = DataStorage(db_file, table_sql)
+db = DataStorage()
 add_user_db = SmallStorage()
